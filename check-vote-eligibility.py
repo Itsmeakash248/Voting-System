00e8ac53d401c0deb542de6cb59f2c77437dc987
Akash <145587932+Itsmeakash248@users.noreply.github.com>
2026-09-12 12:02:17 +0530

    Fix voting eligibility check and add unknown case

--- check-vote-eligibility.py
+++ check-vote-eligibility.py
@@ -2,5 +2,7 @@ age = int(input("Enter your age: "))
 
 if age >= 18:
     print("You are eligible for voting")
-else:
+else if age < 18:
     print("You are not eligible for voting")
+else:
+    print("Unkown Value")
